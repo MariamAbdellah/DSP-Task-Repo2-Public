387fdf2e6d0208a39f3fa2693931fed7566a0fe5
mariam <mariamsalah478@gmail.com>
2024-10-22 21:28:56 +0300

    Task 2 done

--- task1.py
+++ task1.py
@@ -25,19 +25,37 @@ def read_signals(file_path):
     return indices, values
 
 
+def plot_signal(x1, y1, x2, y2,label, label1,label2):
+    window = tk.Toplevel()
+    window.geometry("350x250")
 
-def plot_signal(og_indices, og_values, res_indices, res_values, label):
-    # Plot the signal
-    if (og_values and og_indices):
-        plt.plot(og_indices, og_values, marker='o', linestyle='-', label = "Original Signal")
-    if (res_values and res_indices):
-        plt.plot(res_indices, res_values, marker='x', linestyle='--',label=label)
-    plt.xlabel("Sample Index")
-    plt.ylabel("Amplitude")
-    plt.grid(True)
-    plt.legend()
-    plt.title("Original and " + label)
-    plt.show()
+    # Add the representation combobox
+    tk.Label(window, text="Representation Type:").grid(row=0, column=0, padx=10, pady=5)
+    representation = ttk.Combobox(window, values=["Continuous", "Discrete"])
+    representation.grid(row=0, column=1, padx=10, pady=5)
+    def plot():
+        representation_value = representation.get()  # Get the selected value when plotting
+        if representation_value == 'Continuous':
+            if (isinstance(x1, (list, np.ndarray)) and len(x1) > 0) and (isinstance(y1, (list, np.ndarray)) and len(y1) > 0):
+                plt.plot(x1, y1, marker='o', linestyle='-', label=label1,color='blue',markerfacecolor='orange', markeredgecolor='orange')
+            if (isinstance(x2, (list, np.ndarray)) and len(x2) > 0) and (isinstance(y2, (list, np.ndarray)) and len(y2) > 0):
+                plt.plot(x2, y2, marker='o', linestyle='-', label=label2,color='red',markerfacecolor='red')
+        else:
+            if (isinstance(x1, (list, np.ndarray)) and len(x1) > 0) and (isinstance(y1, (list, np.ndarray)) and len(y1) > 0):
+                plt.stem(x1, y1, linefmt='b-', markerfmt='bo', basefmt=' ', label=label1)
+            if (isinstance(x2, (list, np.ndarray)) and len(x2) > 0) and (isinstance(y2, (list, np.ndarray)) and len(y2) > 0):
+                plt.stem(x2, y2, linefmt='r-', markerfmt='ro', basefmt=' ', label=label2)
+
+        plt.xlabel("Sample Index")
+        plt.ylabel("Amplitude")
+        plt.grid(True)
+        plt.legend()
+        plt.title(label)
+        plt.show()
+    # Add a button to plot the signal
+    plot_button = tk.Button(window, text="Plot", command=plot)
+    plot_button.grid(row=1, column=1, padx=10, pady=10)
+
 def add_signals(signal1, signal2):
     # Extract indices and values from the signal lists
     signal1_indices, signal1_values = signal1
@@ -54,10 +72,6 @@ def add_signals(signal1, signal2):
         addition_result[i] = value1 + value2
     return all_indices, addition_result
 
-
-
-
-
 def sub_signals(signal1, signal2):
 
     values2 = signal2[1]
@@ -67,14 +81,11 @@ def sub_signals(signal1, signal2):
     indices, values = add_signals(signal1, (signal2[0], values2))
     return indices , values
 
-
-
 def delay_advancing_signals(signal1, constant):
     indices = signal1[0]
     indices = [index - constant for index in indices]
     #signal1 = (indices, signal1[1])
     return indices, signal1[1]
-
 
 def folding_signals(signal1):
     indices, values = signal1
@@ -87,8 +98,6 @@ def folding_signals(signal1):
 
     return folded_indices, folded_values
 
-
-
 def multiply_signal(signal1):
     indices, values = signal1
     multiplication_result = []
@@ -96,15 +105,7 @@ def multiply_signal(signal1):
     for value in values:
         multiplication_result.append(value * constant)
     test.MultiplySignalByConst(5,indices,multiplication_result)
-    # plt.plot(indices, values, marker='o', linestyle='-', label="Original Signal")
-    # plt.plot(indices, multiplication_result, marker='x', linestyle='--',label="Multiplied Signal")
-    # plt.xlabel("Sample Index")
-    # plt.ylabel("Amplitude")
-    # plt.grid(True)
-    # plt.legend()
-    # plt.title(f"Original and Multiplied Signal")
-    # plt.show()
-    plot_signal(signal1[0], signal1[1], indices, multiplication_result, "Multiplied Signal")
+    plot_signal(signal1[0], signal1[1], indices, multiplication_result, "Multiplied Signal","Original Signal","Multiplied Signal")
     return indices,multiplication_result
 
 
@@ -112,14 +113,14 @@ def on_signal1_button_click():
     global signal1
     signal1 = read_signals('Signal1.txt')
 def on_displaysignal1_button_click():
-    plot_signal(signal1[0], signal1[1], 0, 0, 'Signal 1')
+    plot_signal(signal1[0], signal1[1], 0, 0, 'Signal 1',"Signal 1","")
 
 def on_signal2_button_click():
     global signal2
     signal2 = read_signals('Signal2.txt')
 
 def on_displaysignal2_button_click():
-    plot_signal(signal2[0], signal2[1], 0, 0, 'Signal 2')
+    plot_signal(signal2[0], signal2[1], 0, 0, 'Signal 2'," Signal 2","")
 
 def on_add_signals_button_click():
     # Add the two signals
@@ -127,25 +128,28 @@ def on_add_signals_button_click():
     test.AddSignalSamplesAreEqual("Signal1.txt", "Signal2.txt",indices, added_values)
 
     # Plot the result
-    plot_signal(0, 0, indices, added_values, 'Signal 1 + Signal 2')
+    plot_signal(0, 0, indices, added_values, 'Addition Result',"","Addition Result")
 
 def on_sub_signals_button_click():
     indeces, values = sub_signals(signal1, signal2)
 
     test.SubSignalSamplesAreEqual("Signal1.txt", "Signal2.txt", indeces, values)
-    plot_signal(0, 0, indeces, values, "Subtraction Result")
+    plot_signal(0, 0, indeces, values, "Subtraction Result"," ",'Subtraction Result')
 
 def on_multiply_signal1_button_click():
     multiply_signal(signal1)
 def on_fold_signal1_button_click():
     indices, values = folding_signals(signal1)
     test.Folding(indices, values)
-    plot_signal(signal1[0], signal1[1], indices, values, "Folded Signal")
+    plot_signal(signal1[0], signal1[1], indices, values, "Folded Signal","Original Signal","Folded Signal")
 def on_delay_advancing_signal1_button_click():
     constant = simpledialog.askfloat("Input", "Enter the constant value to shift the signal:")
     indices, values = delay_advancing_signals(signal1, constant)
     test.ShiftSignalByConst(constant, indices, values)
-    plot_signal(signal1[0], signal1[1], indices, values, "Shifted Signal")
+    plot_signal(signal1[0], signal1[1], indices, values, "Shifted Signal","Original Signal","Shifted Signal")
+
+def on_displaybothsignals_button_click():
+    plot_signal(signal1[0], signal1[1],signal2[0], signal2[1],'Both Signals',"Signal 1",'Signal 2')
 
 def on_generate_signal_button_click():
     window = tk.Toplevel()
@@ -172,57 +176,51 @@ def on_generate_signal_button_click():
     sampling_freq_entry = tk.Entry(window)
     sampling_freq_entry.grid(row=4, column=1, padx=10, pady=5)
 
-    representation_var = tk.StringVar(value = "Continuous")
-    tk.Label(window, text = "Representation:").grid(row = 5, column = 0, padx = 10, pady = 5)
-    continuous_rb = tk.Radiobutton(window, text = "Continuous", variable = representation_var, value = "Continuous")
-    discrete_rb = tk.Radiobutton(window, text = "Discrete", variable = representation_var, value = "Discrete")
-    continuous_rb.grid(row = 5, column = 1, padx = 10, pady = 5, sticky = "w")
-    discrete_rb.grid(row = 5, column = 2, padx = 10, pady = 5)
-
-
     def on_generate():
         signal_type_value = signal_type.get()
         amplitude_value = float(amplitude_entry.get())
         phase_shift_value = float(phase_shift_entry.get())
         analog_freq_value = float(analog_freq_entry.get())
         sampling_freq_value = float(sampling_freq_entry.get())
-        representation_value = representation_var.get()
-
-
-        generate_signal(signal_type_value, amplitude_value, phase_shift_value, analog_freq_value, sampling_freq_value, representation_value)
+        signal, time=generate_signal(signal_type_value, amplitude_value, phase_shift_value, analog_freq_value, sampling_freq_value)
+        plot_signal(time,signal,[],[],'Generate Signal',"Generated Signal","")
 
     tk.Button(window, text = "Generate", command = on_generate).grid(row = 6, column = 1, padx=10, pady=5)
 
 
-def generate_signal(signal_type, amp, phase_shift, analog_freq, sample_freq, representation):
+def generate_signal(signal_type, amp, phase_shift, analog_freq, sample_freq):
 
-    try:
+#    try:
 
-        if sample_freq < 2 * analog_freq:
-            raise ValueError("Invalid sampling frequency value! Sampling frequency should be greater than or equal 2 * analog frequency.")
+    if sample_freq < 2 * analog_freq:
+        messagebox.showerror("Error!!", f"Sampling frequency should be at least {2 * analog_freq} Hz.")
+        return None
+        #raise ValueError("Invalid sampling frequency value! Sampling frequency should be greater than or equal 2 * analog frequency.")
 
+    else :
         t = np.arange(0, 1, 1 / sample_freq)
-
         if signal_type == "Sine":
             signal = amp * np.sin(2 * np.pi * analog_freq * t + phase_shift)
         else:
             signal = amp * np.cos(2 * np.pi * analog_freq * t + phase_shift)
 
-        plt.figure()
+    return signal,t
+        #
+        # plt.figure()
+        #
+        # if representation == "Continuous":
+        #     plt.plot(t, signal, label = f'{signal_type} (Continuous)')
+        # else:
+        #     plt.stem(t, signal, label = f'{signal_type} (Discrete)')  # , linefmt='b-', markerfmt='bo', basefmt="r-")
+        #
+        # plt.title(f'{signal_type} Signal')
+        # plt.xlabel('Time (s)')
+        # plt.ylabel('Amplitude')
+        # plt.grid(True)
+        # plt.show()
 
-        if representation == "Continuous":
-            plt.plot(t, signal, label = f'{signal_type} (Continuous)')
-        else:
-            plt.stem(t, signal, label = f'{signal_type} (Discrete)')  # , linefmt='b-', markerfmt='bo', basefmt="r-")
-
-        plt.title(f'{signal_type} Signal')
-        plt.xlabel('Time (s)')
-        plt.ylabel('Amplitude')
-        plt.grid(True)
-        plt.show()
-
-    except ValueError as e:
-        messagebox.showerror("Invalid Input", str(e))
+  #  except ValueError as e:
+  #      messagebox.showerror("Invalid Input", str(e))
 
 
 
@@ -240,36 +238,41 @@ background_label = tk.Label(root, image=bg_image)
 background_label.place(relwidth=1, relheight=1)
 
 # Create a title label
-title_label = tk.Label(root, text="DSP-Task 1", font=("Helvetica", 32), bg='lightgrey')
-title_label.place(relx=0.5, rely=0.1, anchor='center')  # Center the title label
+# Title Label
+title_label = tk.Label(root, text="DSP-Task", font=("Helvetica", 32), bg='lightgrey')
+title_label.place(relx=0.5, rely=0.04, anchor='center')  # Center the title label
 
-# Create buttons directly in the root window with a transparent effect
+# Buttons
 signal1_button = tk.Button(root, text="Read Signal 1", command=on_signal1_button_click, width=20, height=2, bg='lightgrey', relief='flat')
-signal1_button.place(relx=0.5, rely=0.2, anchor='center')
+signal1_button.place(relx=0.5, rely=0.12, anchor='center')
 
 displaysignal1_button = tk.Button(root, text="Display Signal 1", command=on_displaysignal1_button_click, width=20, height=2, bg='lightgrey', relief='flat')
-displaysignal1_button.place(relx=0.5, rely=0.27, anchor='center')
+displaysignal1_button.place(relx=0.5, rely=0.19, anchor='center')
 
 signal2_button = tk.Button(root, text="Read Signal 2", command=on_signal2_button_click, width=20, height=2, bg='lightgrey', relief='flat')
-signal2_button.place(relx=0.5, rely=0.34, anchor='center')
+signal2_button.place(relx=0.5, rely=0.26, anchor='center')
 
 displaysignal2_button = tk.Button(root, text="Display Signal 2", command=on_displaysignal2_button_click, width=20, height=2, bg='lightgrey', relief='flat')
-displaysignal2_button.place(relx=0.5, rely=0.41, anchor='center')
+displaysignal2_button.place(relx=0.5, rely=0.33, anchor='center')
+
+displaybothsignal_button = tk.Button(root, text="Display Both Signals", command=on_displaybothsignals_button_click, width=20, height=2, bg='lightgrey', relief='flat')
+displaybothsignal_button.place(relx=0.5, rely=0.4, anchor='center')
+
 
 add_signals_button = tk.Button(root, text="Add Signals", command=on_add_signals_button_click, width=20, height=2, bg='lightgrey', relief='flat')
-add_signals_button.place(relx=0.5, rely=0.48, anchor='center')
+add_signals_button.place(relx=0.5, rely=0.47, anchor='center')
 
 sub_signals_button = tk.Button(root, text="Subtract Signals", command=on_sub_signals_button_click, width=20, height=2, bg='lightgrey', relief='flat')
-sub_signals_button.place(relx=0.5, rely=0.55, anchor='center')
+sub_signals_button.place(relx=0.5, rely=0.54, anchor='center')
 
 multiply_signal1_button = tk.Button(root, text="Multiply Signal", command=on_multiply_signal1_button_click, width=20, height=2, bg='lightgrey', relief='flat')
-multiply_signal1_button.place(relx=0.5, rely=0.62, anchor='center')
+multiply_signal1_button.place(relx=0.5, rely=0.61, anchor='center')
 
 fold_signal1_button = tk.Button(root, text="Fold Signal", command=on_fold_signal1_button_click, width=20, height=2, bg='lightgrey', relief='flat')
-fold_signal1_button.place(relx=0.5, rely=0.69, anchor='center')
+fold_signal1_button.place(relx=0.5, rely=0.68, anchor='center')
 
 delay_advancing_signal1_button = tk.Button(root, text="Delay/Advance Signal", command=on_delay_advancing_signal1_button_click, width=20, height=2, bg='lightgrey', relief='flat')
-delay_advancing_signal1_button.place(relx=0.5, rely=0.76, anchor='center')
+delay_advancing_signal1_button.place(relx=0.5, rely=0.75, anchor='center')
 
 generate_signal_button = tk.Button(root, text="Generate Signal", command=on_generate_signal_button_click, width=20, height=2, bg='lightgrey', relief='flat')
 generate_signal_button.place(relx=0.5, rely=0.83, anchor='center')
